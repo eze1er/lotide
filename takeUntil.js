d56c91a18e4e7ed43a238216fec6c52b6c183f0b
eze1er <papaezechiel@gmail.com>
2021-11-26 03:07:12 +0000

    Commit for takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,24 @@
+const takeUntil = function(array, callback) {
+  // ...
+  // create new array
+  let array1 = [];
+
+  // loop through array
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i])) {
+      array1.push(array[i]);
+    } else {
+      return array1;
+    }
+  }
+
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
