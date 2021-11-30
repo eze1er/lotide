5413a7b61a803a93e7763800eb125ab0a9f8885b
eze1er <papaezechiel@gmail.com>
2021-11-30 09:11:43 +0000

    Commit for countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+
+const countLetters = function(firstNames) {
+  let array = firstNames;
+  let object = {};
+
+  for (const elem of array) {
+    if (elem !== ' ') {
+      let counter = 0;
+      for (let i = 0; i < array.length; i++) {
+        if (elem === array[i]) {
+          counter++;
+        }
+      }
+      object[elem] = counter;
+    }
+  }
+  return object;
+};
+const firstNames = "Karl Salima Agouhanna";
+// const firstNames = "Karl";
+countLetters(firstNames);
